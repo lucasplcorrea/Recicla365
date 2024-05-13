a31e59abc862ad9aadce6e04bd6d88dfd643aa1b
Lucas Pedro <lucaspedrolopescorrea@gmail.com>
2024-05-13 15:14:18 -0300

    Adiciona função de exclusão

--- recicla365/src/pages/locaisDeColetaAdmin/index.jsx
+++ recicla365/src/pages/locaisDeColetaAdmin/index.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Grid, Card, CardContent, Typography, Modal, TextField, Button, FormControlLabel, Checkbox, FormGroup, Box } from '@mui/material';
+import { Container, Grid, Card, CardContent, Typography, Modal, TextField, Button, FormControlLabel, Checkbox, FormGroup, Box, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
 
 const ListaLocais = () => {
   const [locais, setLocais] = useState([]);
   const [modalAberto, setModalAberto] = useState(false);
   const [localSelecionado, setLocalSelecionado] = useState({});
   const [loading, setLoading] = useState(true);
+  const [confirmacaoExclusaoAberta, setConfirmacaoExclusaoAberta] = useState(false);
 
   useEffect(() => {
     const fetchLocais = async () => {
@@ -79,6 +80,31 @@ const ListaLocais = () => {
     }
   };
 
+  const handleAbrirConfirmacaoExclusao = (local) => {
+    setLocalSelecionado(local);
+    setConfirmacaoExclusaoAberta(true);
+  };
+
+  const handleFecharConfirmacaoExclusao = () => {
+    setConfirmacaoExclusaoAberta(false);
+  };
+
+  const handleConfirmarExclusao = async () => {
+    try {
+      const response = await fetch(`http://localhost:5000/locaisDeColeta/${localSelecionado.id}`, {
+        method: 'DELETE',
+      });
+      if (!response.ok) {
+        throw new Error('Erro ao excluir local de coleta');
+      }
+      const updatedLocais = locais.filter(local => local.id !== localSelecionado.id);
+      setLocais(updatedLocais);
+      setConfirmacaoExclusaoAberta(false);
+    } catch (error) {
+      console.error('Erro ao excluir local de coleta:', error);
+    }
+  };
+
   if (loading) {
     return <div>Carregando...</div>;
   }
@@ -97,6 +123,7 @@ const ListaLocais = () => {
                   {local.descricao}
                 </Typography>
                 <Button onClick={() => abrirModalEditar(local)}>Editar</Button>
+                <Button onClick={() => handleAbrirConfirmacaoExclusao(local)} color="error">Excluir</Button>
               </CardContent>
             </Card>
           </Grid>
@@ -105,45 +132,60 @@ const ListaLocais = () => {
 
       {/* Modal de edição */}
       <Modal open={modalAberto} onClose={fecharModal}>
-        <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'white', padding: '20px', borderRadius: '8px', width: '80%', maxWidth: '600px', overflowY: 'auto', maxHeight: '80vh' }}>
-          <Typography variant="h5" gutterBottom>Editar Local de Coleta</Typography>
-          <form>
-            {localSelecionado && Object.keys(localSelecionado).map(key => {
-              if (key !== 'id' && key !== 'identificadorUsuario' && key !== 'nomeUsuario' && key !== 'tiposResiduos') {
-                return (
-                  <TextField
-                    key={key}
-                    name={key}
-                    label={key.charAt(0).toUpperCase() + key.slice(1)}
-                    value={localSelecionado[key] || ''}
-                    onChange={handleInputChange}
-                    fullWidth
-                    style={{ marginBottom: '10px' }}
-                  />
-                );
-              }
-              return null;
-            })}
-            {/* Checkboxes para tipos de resíduos */}
-            <Typography variant="subtitle1" gutterBottom>Tipos de Resíduos</Typography>
-            <FormGroup>
-              {localSelecionado && localSelecionado.tiposResiduos && Object.entries(localSelecionado.tiposResiduos).map(([key, value]) => (
-                <FormControlLabel
-                  key={key}
-                  control={<Checkbox
-                    name={key}
-                    checked={value}
-                    onChange={handleCheckboxChange}
-                  />}
-                  label={key.charAt(0).toUpperCase() + key.slice(1)}
-                />
-              ))}
-            </FormGroup>
-            <Button onClick={handleSalvarEdicao} variant="contained" style={{ marginRight: '10px' }}>Salvar</Button>
-            <Button onClick={fecharModal} variant="contained">Cancelar</Button>
-          </form>
-        </Box>
-      </Modal>
+  <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'white', padding: '20px', borderRadius: '8px', width: '80%', maxWidth: '600px', overflowY: 'auto', maxHeight: '80vh' }}>
+    <Typography variant="h5" gutterBottom>Editar Local de Coleta</Typography>
+    {localSelecionado && (
+      <form>
+        {Object.keys(localSelecionado).map(key => {
+          if (key !== 'id' && key !== 'identificadorUsuario' && key !== 'nomeUsuario' && key !== 'tiposResiduos') {
+            return (
+              <TextField
+                key={key}
+                name={key}
+                label={key.charAt(0).toUpperCase() + key.slice(1)}
+                value={localSelecionado[key] || ''}
+                onChange={handleInputChange}
+                fullWidth
+                style={{ marginBottom: '10px' }}
+              />
+            );
+          }
+          return null;
+        })}
+        {/* Checkboxes para tipos de resíduos */}
+        <Typography variant="subtitle1" gutterBottom>Tipos de Resíduos</Typography>
+        <FormGroup>
+          {localSelecionado && localSelecionado.tiposResiduos && Object.entries(localSelecionado.tiposResiduos).map(([key, value]) => (
+            <FormControlLabel
+              key={key}
+              control={<Checkbox
+                name={key}
+                checked={value}
+                onChange={handleCheckboxChange}
+              />}
+              label={key.charAt(0).toUpperCase() + key.slice(1)}
+            />
+          ))}
+        </FormGroup>
+        <Button onClick={handleSalvarEdicao} variant="contained" style={{ marginRight: '10px' }}>Salvar</Button>
+        <Button onClick={fecharModal} color="error" variant="contained">Cancelar</Button>
+      </form>
+    )}
+  </Box>
+</Modal>
+
+
+      {/* Modal de confirmação de exclusão */}
+      <Dialog open={confirmacaoExclusaoAberta} onClose={handleFecharConfirmacaoExclusao}>
+        <DialogTitle>Confirmar Exclusão</DialogTitle>
+        <DialogContent>
+          <Typography variant="body1">Tem certeza de que deseja excluir este local de coleta?</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleFecharConfirmacaoExclusao}>Cancelar</Button>
+          <Button onClick={handleConfirmarExclusao} color="error">Excluir</Button>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 };
